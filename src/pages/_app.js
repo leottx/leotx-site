ebc41e2056e82744ea589b0486f0cd69ea9f69ab
leotx <tteixeira.leonardo@gmail.com>
2023-02-14 14:33:41 -0300

    refactor: remove um classe css do container da aplicacao

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ import "@/global/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="bg-zinc-50 h-screen">
+    <div className="bg-zinc-50">
       <div className="mx-auto max-w-screen-lg px-4">
         <Header />
         <Component {...pageProps} />
