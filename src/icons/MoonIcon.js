b244a68428264d5d305d2f00d07998ce29225afc
leotx <tteixeira.leonardo@gmail.com>
2023-02-14 10:19:35 -0300

    refactor: change MoonIcon and SunIcon to another directory

--- src/icons/MoonIcon.js
+++ src/icons/MoonIcon.js
@@ -2,8 +2,8 @@ export default function MoonIcon(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={props.size}
-      height={props.size}
+      width={props?.size}
+      height={props?.size}
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
