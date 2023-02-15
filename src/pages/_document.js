2296b8237d53aed05a8a52b5bae1eba9164e7e74
leotx <tteixeira.leonardo@gmail.com>
2023-02-15 13:35:50 -0300

    feat: adiciona uma mensagem na tag html de todas as paginas

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html
+      lang="en"
+      hello="Hey there! All good? 🇧🇷✨🐱. My website is   still under construction, but feel free to explore 👀"
+    >
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
