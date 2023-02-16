ec2234fd12a97ca7a23b6dc39ea5ffc57f7084bd
leotx <tteixeira.leonardo@gmail.com>
2023-02-16 17:22:56 -0300

    fix: altera os estilos do componente SectionTitle

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function SectionTitle({ itemNumber, itemText }) {
   return (
-    <h2 className="flex whitespace-nowrap items-center text-xl mb-4 after:content-[''] after:relative after:h-[1px] after:w-full after:bg-zinc-200 after:block after:ml-3">
-      <span className="font-mono text-zinc-500 mr-1">{itemNumber}</span>
+    <h2 className="flex whitespace-nowrap items-baseline text-xl mb-4 after:content-[''] after:relative after:h-[1px] after:w-full after:bg-zinc-200 after:block after:ml-3 after:self-center">
+      <span className="font-mono text-sm text-zinc-500 mr-[2px]">
+        {itemNumber}
+      </span>
       <span className="font-bold text-zinc-900">{itemText}</span>
     </h2>
   );
