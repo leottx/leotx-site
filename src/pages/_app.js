962a5e3c0dc9abeeca4c02d5cba2cef1f54a4e89
leotx <tteixeira.leonardo@gmail.com>
2023-02-16 17:22:20 -0300

    fix: altera a estrutura eos estilos do container geral da aplicacao

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,11 +8,13 @@ import "@/styles/main.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="bg-zinc-50">
-      <div className="mx-auto max-w-screen-lg pt-6 px-5">
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
+    <div className="bg-zinc-50 sm:px-8 lg:px-16">
+      <div className="mx-auto bg-white max-w-[1216px] pt-6 px-8 lg:px-12">
+        <div className="max-w-screen-lg mx-auto">
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </div>
       </div>
     </div>
   );
