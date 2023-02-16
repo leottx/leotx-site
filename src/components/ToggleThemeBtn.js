f61248f50f5a41d68ff80687f25f4cb52a01a296
leotx <tteixeira.leonardo@gmail.com>
2023-02-16 17:24:15 -0300

    fix: altera os estilos do componente ToggleThemeBtn

--- src/components/ToggleThemeBtn.js
+++ src/components/ToggleThemeBtn.js
@@ -10,7 +10,7 @@ export default function ToggleThemeBtn() {
 
   return (
     <button
-      className="w-10 h-10 flex items-center justify-center rounded-lg transition-colors duration-200 hover:bg-teal-100/60"
+      className="w-10 h-10 flex items-center justify-center rounded-lg transition-colors duration-200 hover:bg-teal-50"
       onClick={() => setDarkMode((currentState) => !currentState)}
     >
       {!isDarkMode ? <SunIcon size="18" /> : <MoonIcon size="18" />}
