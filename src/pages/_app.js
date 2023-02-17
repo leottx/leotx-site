1e28de1ae441b6132ea25b307ca5a12de601cd94
leotx <tteixeira.leonardo@gmail.com>
2023-02-17 14:43:38 -0300

    fix: corrige a altura do container geral da aplicacao

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ import "@/styles/main.css";
 export default function App({ Component, pageProps }) {
   return (
     <div className="bg-zinc-50 sm:px-8 lg:px-16 bg-waves-texture bg-[length:12px]">
-      <div className="mx-auto bg-white max-w-[1216px] py-6 px-5 sm:px-8 lg:px-12 border border-zinc-200/50">
+      <div className="mx-auto bg-white max-w-[1216px] py-6 px-5 sm:px-8 lg:px-12 border border-zinc-200/50 min-h-screen">
         <div className="max-w-screen-lg mx-auto">
           <Header />
           <Component {...pageProps} />
