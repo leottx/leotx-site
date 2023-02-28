3266d12b484c98282773bfbc18125dae01128578
leotx <tteixeira.leonardo@gmail.com>
2023-02-28 08:42:59 -0300

    chore: instala as dependencias implementacao do blog utilizando mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  // webpack(config, { nextRuntime }) {
+  //   // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
+  //   if (typeof nextRuntime === "undefined") {
+  //     config.resolve.fallback = {
+  //       ...config.resolve.fallback,
+  //       fs: false,
+  //     };
+  //   }
 
-module.exports = nextConfig
+  //   return config;
+  // },
+};
+
+module.exports = withMDX(nextConfig);
